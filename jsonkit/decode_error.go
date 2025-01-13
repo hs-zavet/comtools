1494c8060beb7b16c6a9a8d3945f627830aa37c9
kish1n <yehorushakov@gmail.com>
2025-01-13 02:40:51 +0200

    add json kit

---
+++ jsonkit/decode_error.go
@@ -0,0 +1,13 @@
+package jsonkit
+
+import (
+	"fmt"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+)
+
+func newDecodeError(what string, err error) error {
+	return validation.Errors{
+		what: fmt.Errorf("decode request %s: %w", what, err),
+	}
+}
